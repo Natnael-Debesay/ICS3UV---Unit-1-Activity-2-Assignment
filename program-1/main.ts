eb96eb8cb514ed361936ad6102de4f7e02a9ed57
default <natnael.debesay@stu.ocsb.ca>
2025-10-03 17:28:51 +0000

    main.ts

---
+++ program-1/main.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Natnael Debesay
+ * @version 1.0.0
+ * @date 2025-10-03
+ * @fileoverview This program converts 34*C to its Fahrenheight qeuivalent.
+ */
+
+//Using the formula: F= 9 / 5 * C + 32
+console.log("9 / 5 * 34 + 32 = " + String(9/5*34+32));
+
+console.log("\nDone.");
