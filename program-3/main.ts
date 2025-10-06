499f3de6b103813d5641af427b553537dc2091f2
default <natnael.debesay@stu.ocsb.ca>
2025-10-06 20:40:18 +0000

    main.ts

---
+++ program-3/main.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Natnael Debesay
+ * @version 1.0.0
+ * @date 2025-10-06
+ * @fileoverview This program outputs the volume of a sphere with a radius of 4 cm.
+ */
+
+// Using the formula: V = 4/3 * Pi * r * r * r
+console.log("4 / 3 * 3.14 * 4 * 4 * 4 = " + String(4 / 3 * 3.14 * 4 * 4 * 4));
+
+console.log("\nDone.");
