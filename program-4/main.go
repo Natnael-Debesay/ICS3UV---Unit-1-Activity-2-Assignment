c662583e417f3470ab17bdcf721099a3caa4d5a4
default <natnael.debesay@stu.ocsb.ca>
2025-10-06 21:03:28 +0000

    main.go

--- program-4/main.go
+++ program-4/main.go
@@ -1,1 +1,17 @@
-package program4
+/*
+ * @author Natnael Debesay
+ * @version 1.0.0
+ * @date 2025-10-06
+ * @fileoverview This program calculates the gross pay for an employee named Fred.
+ */
+
+package main
+
+import "fmt"
+
+func main () {
+	//Using the formula: gross pay = hours worked * hourly wage
+	fmt.Println("40 * 8.20 = " + fmt.Sprint(40*8.20))
+
+	fmt.Println("\nDone.")
+}
