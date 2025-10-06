3ccd6ac95da5acb0084e91ac4a4f17fded175d40
default <natnael.debesay@stu.ocsb.ca>
2025-10-06 20:28:53 +0000

    main.go

---
+++ program-2/main.go
@@ -0,0 +1,17 @@
+/*
+ * @author Natnael Debesay
+ * @version 1.0.0
+ * @date 2025-10-06
+ * @fileoverview This program computes and outputs the area of a circle with a radius of 14 cm.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	// Using the formula: A = Pi * r * r
+	fmt.Println("3.14 * 14 * 14 = " + fmt.Sprint(3.14*14*14))
+
+	fmt.Println("\nDone.")
+}
---
+++ program-2/main.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Natnael Debesay
+ * @version 1.0.0
+ * @date 2025-10-06
+ * @fileoverview This program computes and outputs the area of a circie with a radius of 14 cm.
+ */
+
+// Using the formula: A = Pi * r * r
+console.log("3.14 * 14 * 14 = " + String(3.14 * 14 * 14));
+
+console.log("\nDone.");
