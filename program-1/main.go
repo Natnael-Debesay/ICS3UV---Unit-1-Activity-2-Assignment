90ba7830fd97ed3983c8fb4978cc9482ffae911d
default <natnael.debesay@stu.ocsb.ca>
2025-10-06 16:37:30 +0000

    main.go

--- program-1/main.go
+++ program-1/main.go
@@ -1,1 +1,17 @@
+/*
+ * @author Natnael Debesay
+ * @version 1.0.0
+ * @date 2025-10-03
+ * @fileoverview This program converts 34*C to its Fahrenheight equivalent
+ */
 
+package main
+
+import "fmt"
+
+func main() {
+	// Using the formula: F = 9 / 5 * C + 32
+	fmt.Println("9 / 5 * 34 + 32 = " + fmt.Sprint(9/5*34+32))
+
+	fmt.Println("\nDone.")
+}
