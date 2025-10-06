95333f61939c3a70986e5730c9fdb405c4c2c3b7
default <natnael.debesay@stu.ocsb.ca>
2025-10-06 20:55:06 +0000

    main.ts

---
+++ program-4/main.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Natnael Debesay
+ * @version 1.0.0
+ * @date 2025-10-06
+ * @fileoverview This program calculates the gross pay for an employee named Fred.
+ */
+
+// Using the formula: gross pay = hours worked * hourly wage
+console.log("40 * 8.20 = " + String(40 * 8.20));
+
+console.log("\nDone.")
