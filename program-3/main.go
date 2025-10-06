711ab6ba66f098aaac418a229a8057252c909e9a
default <natnael.debesay@stu.ocsb.ca>
2025-10-06 20:48:29 +0000

    main.go

--- program-3/main.go
+++ program-3/main.go
@@ -1,1 +1,17 @@
-package program3
+/*
+ * @author Natnael Debesay
+ * @version 1.0.0
+ * @date 2025-10-06
+ * @fileoverview This program outputs the volume of a sphere with a radius of 4 cm.
+ */
+
+package main
+
+import "fmt"
+
+func main () {
+	//Using the formula: V = 4/3 * Pi * r * r * r
+	fmt.Println("4 / 3 * 3.14 * 4 * 4 * 4 = " + fmt.Sprint(4/3*3.14*4*4*4))
+
+	fmt.Println("\nDone.")
+}
