5158d7e08c7498354a399445ffef19d8412626c0
default <natnael.debesay@stu.ocsb.ca>
2025-10-09 14:46:06 +0000

    main.go

--- program-1/main.go
+++ program-1/main.go
@@ -2,7 +2,7 @@
  * @author Natnael Debesay
  * @version 1.0.0
  * @date 2025-10-03
- * @fileoverview This program converts 34*C to its Fahrenheight equivalent
+ * @fileoverview This program converts 34*C to its Fahrenheight equivalent.
  */
 
 package main
