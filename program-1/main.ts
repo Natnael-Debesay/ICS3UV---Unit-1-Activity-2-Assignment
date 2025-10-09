3d34bdbb98c3265f35408e7172ece5e2a96cbdf5
default <natnael.debesay@stu.ocsb.ca>
2025-10-09 14:40:11 +0000

    main.ts

--- program-1/main.ts
+++ program-1/main.ts
@@ -2,7 +2,7 @@
  * @author Natnael Debesay
  * @version 1.0.0
  * @date 2025-10-03
- * @fileoverview This program converts 34*C to its Fahrenheight qeuivalent.
+ * @fileoverview This program converts 34*C to its Fahrenheit equivalent.
  */
 
 //Using the formula: F= 9 / 5 * C + 32
